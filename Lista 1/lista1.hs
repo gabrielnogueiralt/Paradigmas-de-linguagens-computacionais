46bbb7c28455e3794da1cec0c6b93df1176478c5
gnl2 <gnl2@cin.ufpe.br>
2021-07-23 13:23:16 -0300

    Questão: Junte as listas.

--- Lista 1/lista1.hs
+++ Lista 1/lista1.hs
@@ -2,3 +2,10 @@
 potencia :: Integer -> Integer -> Integer
 potencia n 0 = 1
 potencia n k = n * potencia n (k-1)
+
+-- Junte as listas
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] x = x
+merge x [] = x
+merge (x:xs) (y:ys) | y < x     = y : merge (x:xs) ys
+merge (x:xs) (y:ys) | otherwise = x : merge xs (y:ys)
